5ea55f783797738541abfdf0af285c6e1e42fc69
mb5ss95 <mb3ss@naver.com>
2022-09-07 17:58:23 +0900

    220907 야오~

---
+++ 220907_vuejs/vue_cli_start/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+        <h1>App</h1>
+        <Header/>
+        <Main/>
+    </div>
+</template>
+
+<script>
+import Header from "./components/Header"
+import Main from "./components/Main"
+export default{
+    components:{
+        Header,
+        Main,
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
